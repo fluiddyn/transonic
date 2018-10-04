8cf8c71723f85b9455b0c19e36718c60f24f45f6
paugier <pierre.augier@ens-lyon.org>
2018-10-04 17:10:23 +0200

    No fluiddyn in the tests

--- data_tests/classic.py
+++ data_tests/classic.py
@@ -1,8 +1,6 @@
 
 import numpy as np
 
-from fluiddyn.util import mpi
-
 from fluidpythran import FluidPythran
 
 fp = FluidPythran()
@@ -19,21 +17,3 @@ fp = FluidPythran()
 @fp.pythranize
 def func(a, b):
     return (a * np.log(b)).max()
-
-
-if __name__ == "__main__":
-
-    n0, n1 = 100, 200
-    a0 = np.random.rand(n0, n1)
-    a1 = np.random.rand(n0, n1)
-
-    result = func(a0, a1)
-    if mpi.nb_proc > 1:
-        result = mpi.comm.allreduce(result, op=mpi.MPI.MAX)
-    mpi.printby0(result)
-
-    a0 = (1000 * a0).astype(int)
-    result = func(a0, a1)
-    if mpi.nb_proc > 1:
-        result = mpi.comm.allreduce(result, op=mpi.MPI.MAX)
-    mpi.printby0(result)
