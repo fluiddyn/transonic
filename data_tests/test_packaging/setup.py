a6c65d0dd12cd425ace9cb67439eeb2cca351c5c
Ashwin Vishnu <avmo@kth.se>
2019-01-30 11:51:36 +0100

    Make backend files while executing test_build_ext

--- data_tests/test_packaging/setup.py
+++ data_tests/test_packaging/setup.py
@@ -1,17 +1,17 @@
 from distutils.core import setup, Extension
 from pathlib import Path
 import numpy as np
-from transonic.dist import init_pythran_extensions, ParallelBuildExt
+from transonic.dist import make_backend_files, init_pythran_extensions, ParallelBuildExt
 from transonic.compat import fspath
 
 
+path_sources = Path(__file__).parent.absolute()
 include_dirs = [np.get_include()]
 
 try:
     from Cython.Build import cythonize
 except ImportError:
     # from setuptools import Extension
-    path_sources = Path(__file__).parent.absolute()
     extensions = [
         Extension(
             "add_cython",
@@ -27,6 +27,7 @@ else:
         Extension("add_cython", ["add_cython.pyx"], include_dirs=include_dirs)
     )
 
+make_backend_files([path_sources / "add.py"])
 extensions.extend(init_pythran_extensions(".", include_dirs=include_dirs))
 
 setup(
