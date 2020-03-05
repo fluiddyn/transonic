e31913a6e3d8a6594eb7ad3231a1cc6f6bfb5669
Ashwin Vishnu <avmo@kth.se>
2020-03-05 11:12:53 +0100

    Save backend python for data test subpackages

---
+++ data_tests/saved__backend__/python/subpackages.py
@@ -0,0 +1,19 @@
+from numpy.fft import rfft
+from numpy.random import randn
+from numpy.linalg import matrix_power
+
+
+def test_np_fft(u):
+    u_fft = rfft(u)
+    return u_fft
+
+
+def test_np_linalg_random(u):
+    (nx, ny) = u.shape
+    u[:] = randn(nx, ny)
+    u2 = u.T * u
+    u4 = matrix_power(u2, 2)
+    return u4
+
+
+__transonic__ = ("0.4.2",)
