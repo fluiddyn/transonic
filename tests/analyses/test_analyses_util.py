91613e8f60c496f3074ae571429624f694ae05c3
paugier <pierre.augier@univ-grenoble-alpes.fr>
2025-09-10 01:14:19 +0200

    test: xfail test_analyses_util.py::test_print_dumped

--- tests/analyses/test_analyses_util.py
+++ tests/analyses/test_analyses_util.py
@@ -1,3 +1,7 @@
+import sys
+
+import pytest
+
 from transonic.analyses.extast import parse
 from transonic.analyses.util import print_dumped, print_unparsed
 
@@ -5,6 +9,11 @@ source = "def func(): return True"
 
 
 def test_print_dumped():
+
+    if sys.version_info > (3, 13, 3):
+        pytest.xfail("bug gast 0.6.0 for Python >= 3.13.4 (but should work)")
+        # see https://github.com/serge-sans-paille/gast/issues/95
+
     print_dumped(source)
 
 
