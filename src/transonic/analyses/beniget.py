8ff7d99a44484871061ce9d7591750d8144d462f
paugier <pierre.augier@univ-grenoble-alpes.fr>
2025-12-08 13:57:51 +0100

    analyses: support beniget 0.5.0

--- src/transonic/analyses/beniget.py
+++ src/transonic/analyses/beniget.py
@@ -1,4 +1,7 @@
-import gast as ast
+import ast as _ast
+
+import gast
+from beniget import beniget as _beniget
 from beniget import Ancestors
 from beniget import DefUseChains as DUC
 from beniget import UseDefChains
@@ -7,9 +10,25 @@ from beniget.beniget import Def
 __all__ = ["Ancestors", "DefUseChains", "UseDefChains"]
 
 
+def _pkg(node):
+    """
+    Given a supported AST node, return the origin module where it's class is defined.
+    The result will be gast or ast.
+    """
+    module_name = node.__class__.__module__
+    return (
+        gast.gast
+        if module_name in ["gast.gast", "transonic.analyses.extast"]
+        else _ast
+    )
+
+
+_beniget.pkg = _pkg
+
+
 class DefUseChains(DUC):
     def visit_List(self, node):
-        if isinstance(node.ctx, ast.Load):
+        if isinstance(node.ctx, gast.Load):
             dnode = self.chains.setdefault(node, Def(node))
             for elt in node.elts:
                 if isinstance(elt, CommentLine):
@@ -18,7 +37,7 @@ class DefUseChains(DUC):
             return dnode
         # unfortunately, destructured node are marked as Load,
         # only the parent List/Tuple is marked as Store
-        elif isinstance(node.ctx, ast.Store):
+        elif isinstance(node.ctx, gast.Store):
             return self.visit_Destructured(node)
 
     visit_Tuple = visit_List
